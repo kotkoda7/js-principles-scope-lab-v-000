631b61a49cbb521a75936245a3b53bd7c30236f7
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-11-05 03:44:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 var customerName = 'bob';
 
-leastFavoriteCustomer
+const = leastFavoriteCustomer
 
 function upperCaseCustomerName() {
   customerName = customername.toUpperCase;
