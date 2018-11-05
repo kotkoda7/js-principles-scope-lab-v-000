417f89d05af4b24e5ea964ab4c5cbbaf3f758c9f
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-11-05 03:49:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var customerName = 'bob';
 const leastFavoriteCustomer = 'name';
 
 function upperCaseCustomerName() {
-  customerName = customerName.toUpperCase;
+  customerName = customerName.toUpperCase();
 }
 
 function setBestCustomer() {
