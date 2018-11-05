45673d521e81b6d74b923581ca9208cac6bf4ac3
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-11-05 03:40:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customername.toUpperCase;
+}
+
+function setBestCustomer() {
+  let bestCustomer = 'not bob';
+}
+
+overwriteBestCustomer()a
